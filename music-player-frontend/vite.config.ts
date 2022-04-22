43b61d0e6db65309fa234da25053d77d6a713306
TenViki <viktorkomarek28@gmail.com>
2022-04-22 20:27:39 +0200

    :fire: Removes the useless stuff

--- music-player-frontend/vite.config.ts
+++ music-player-frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "asd",
-  },
-  plugins: [react(), mkcert()],
+  plugins: [react()],
 });
