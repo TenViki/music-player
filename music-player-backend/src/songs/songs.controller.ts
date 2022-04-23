b8aa529573ed69a4b68a7113a37bcf383fb945cd
TenViki <viktorkomarek28@gmail.com>
2022-04-23 11:13:21 +0200

    :zap: Makes upload endpoint guarded

--- music-player-backend/src/songs/songs.controller.ts
+++ music-player-backend/src/songs/songs.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { CurrentUser } from "src/auth/middleware/current-user.middleware";
 import { User } from "src/entities/user.entity";
+import { AuthGuard } from "src/guards/auth.guard";
 import { SongsService } from "./songs.service";
 import { UploadSongDto } from "./upload-song.dto";
 
@@ -9,6 +10,7 @@ export class SongsController {
   constructor(private songsService: SongsService) {}
 
   @Post("/")
+  @UseGuards(AuthGuard)
   async uploadSong(
     @Body() songObject: UploadSongDto,
     @CurrentUser() user: User,
