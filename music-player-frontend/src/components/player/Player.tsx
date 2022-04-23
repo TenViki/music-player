328a3718ce8a38e104ae3ded337f2bb4b275dc95
TenViki <viktorkomarek28@gmail.com>
2022-04-23 21:45:33 +0200

    :sparkles: Adds toggling to playlist

--- music-player-frontend/src/components/player/Player.tsx
+++ music-player-frontend/src/components/player/Player.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Song } from "../../api/songs";
+import { getImageCover } from "../../utils/songs";
+import { FiChevronDown } from "react-icons/fi";
+import "./player.scss";
 
 interface PlayerProps {
   currentSong?: Song;
@@ -10,9 +13,43 @@ const Player: React.FC<PlayerProps> = ({ currentSong, playlist }) => {
   const [collapsed, setCollapsed] = React.useState(true);
 
   return (
-    <div className={`player ${collapsed ? "collapsed" : ""}`}>
+    <div
+      className={`player ${collapsed ? "collapsed" : ""} ${
+        currentSong ? "active" : ""
+      }`}
+    >
+      <div className="player-background">
+        {currentSong?.cover && (
+          <img
+            className="player-background-image"
+            src={currentSong.cover}
+            alt=""
+          />
+        )}
+      </div>
       <div className="player-header">
-        <div className="player-header-cover">{currentSong?.cover}</div>
+        <div className="player-header-cover">
+          {getImageCover(currentSong?.cover || "")}
+        </div>
+        <div className="player-header-info">{currentSong?.title}</div>
+        <div
+          className="playlist-header-toggle"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          <FiChevronDown />
+        </div>
+      </div>
+
+      <div className="player-content">
+        <div className="player-song">
+          <div className="player-cover">
+            {getImageCover(currentSong?.cover || "")}
+          </div>
+          <div className="player-info">
+            <div className="player-title">{currentSong?.title}</div>
+            <div className="player-artist">{currentSong?.artist}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
