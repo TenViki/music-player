31483587c6fa3459cc8d421715558fc946a5acb1
TenViki <viktorkomarek28@gmail.com>
2022-04-23 21:58:49 +0200

    :sparkles: Adds route for downloading files

--- music-player-backend/src/songs/songs.controller.ts
+++ music-player-backend/src/songs/songs.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  StreamableFile,
+  UseGuards,
+} from "@nestjs/common";
 import { CurrentUser } from "src/auth/middleware/current-user.middleware";
 import { User } from "src/entities/user.entity";
 import { AuthGuard } from "src/guards/auth.guard";
@@ -6,6 +15,9 @@ import { Serialize } from "src/interceptors/serialize.interceptor";
 import { SongsService } from "./songs.service";
 import { SpotifySongDto } from "./spotify-song.dto";
 import { AddSongDto } from "./upload-song.dto";
+import * as fs from "fs/promises";
+import { createReadStream } from "fs";
+import { join } from "path";
 
 @Controller("songs")
 export class SongsController {
@@ -21,6 +33,20 @@ export class SongsController {
   @UseGuards(AuthGuard)
   getAll(@CurrentUser() user: User) {
     return this.songsService.getAll(user);
+  }
+
+  @Get("/:file.mp3")
+  async getSong(@Param("file") filename: string) {
+    console.log(filename);
+    try {
+      await fs.access("./files/" + filename + ".mp3");
+      const file = createReadStream(
+        join(process.cwd(), "files", filename + ".mp3"),
+      );
+      return new StreamableFile(file);
+    } catch (error) {
+      throw new NotFoundException("File not found");
+    }
   }
 
   @Get("/search/:q")
