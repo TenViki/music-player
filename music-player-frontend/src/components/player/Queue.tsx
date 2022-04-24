3d819c675f0d4c1b74af3e60b07597c3d2754ef5
TenViki <viktorkomarek28@gmail.com>
2022-04-24 08:27:03 +0200

    :zap: Styles Queue

--- music-player-frontend/src/components/player/Queue.tsx
+++ music-player-frontend/src/components/player/Queue.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Song } from "../../api/songs";
+import PlaylistEntry from "../playlist/PlaylistEntry";
 
 interface QueueProps {
   queue: Song[];
@@ -7,9 +8,9 @@ interface QueueProps {
 
 const Queue: React.FC<QueueProps> = ({ queue }) => {
   return (
-    <div>
+    <div className="player-queue">
       {queue.map((song, index) => (
-        <div key={index}>{song.title}</div>
+        <PlaylistEntry onSelect={() => {}} song={song} key={index} />
       ))}
     </div>
   );
