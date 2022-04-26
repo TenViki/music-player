bcdb231c9b9020316fbbcefbd3cea316d4c332bb
TenViki <34690804+TenViki@users.noreply.github.com>
2022-04-26 22:59:27 +0200

    :bug: Fixes undefined cover

--- music-player-frontend/src/components/playlist/PlaylistEntry.tsx
+++ music-player-frontend/src/components/playlist/PlaylistEntry.tsx
@@ -26,7 +26,7 @@ const PlaylistEntry: React.FC<PlaylistEntryProps> = ({ song, onSelect }) => {
 
   return (
     <div className="playlist-entry" onClick={handleClick}>
-      <div className="playlist-entry-cover">{getImageCover(song.cover)}</div>
+      <div className="playlist-entry-cover">{getImageCover(song?.cover)}</div>
       <div className="playlist-entry-info">
         <div className="playlist-entry-title">{song.title}</div>
         <div className="playlist-entry-artist">{song.artist}</div>
